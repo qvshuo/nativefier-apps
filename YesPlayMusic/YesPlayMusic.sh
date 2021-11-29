bdc63165dc02950ad25b27147f594388d7c4a84f
杨蕙芷 <qvshuo@foxmail.com>
2021-11-29 13:31:26 +0800

    Update YesPlayMusic.sh

--- YesPlayMusic/YesPlayMusic.sh
+++ YesPlayMusic/YesPlayMusic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nativefier --conceal --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --single-instance 'https://yes-play-music-gules-five.vercel.app/'
+nativefier --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --single-instance 'https://yes-play-music-gules-five.vercel.app/'
 
 tar -zcvf YesPlayMusic-linux-x64.tar.gz YesPlayMusic-linux-x64
