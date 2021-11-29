27ba6013333fc8e5080d577f1f4e64f2513c1736
杨蕙芷 <qvshuo@foxmail.com>
2021-11-29 16:19:16 +0800

    Update WeChat.sh

--- WeChat/WeChat.sh
+++ WeChat/WeChat.sh
@@ -2,3 +2,4 @@
 
 nativefier --icon 'WeChat.png' --name 'WeChat' --single-instance --file-download-options '{"saveAs": true}' --inject 'WeChat.css' --inject 'WeChat.js' --internal-urls ".*?" 'https://wx.qq.com/'
 
+tar -zcvf WeChat-linux-x64.tar.gz WeChat-linux-x64
