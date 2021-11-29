36b530f372712cf1af28233b499954c536408ddb
杨蕙芷 <qvshuo@foxmail.com>
2021-11-29 10:24:27 +0800

    Update YesPlayMusic.sh

--- YesPlayMusic/YesPlayMusic.sh
+++ YesPlayMusic/YesPlayMusic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nativefier --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --portable --single-instance 'https://yes-play-music-git-master-qier222.vercel.app/'
+nativefier --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --portable --single-instance 'https://yes-play-music-gules-five.vercel.app/'
