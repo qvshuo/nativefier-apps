c7536a17f71f465c1c9fd17695d4c588ae7d2dce
杨蕙芷 <qvshuo@foxmail.com>
2021-11-29 16:18:07 +0800

    Update WeChat.sh

--- WeChat/WeChat.sh
+++ WeChat/WeChat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nativefier --icon 'WeChat.png' --name 'WeChat' --portable --single-instance --file-download-options '{"saveAs": true}' --inject 'WeChat.css' --inject 'WeChat.js' --internal-urls ".*?" 'https://wx.qq.com/'
+nativefier --icon 'WeChat.png' --name 'WeChat' --single-instance --file-download-options '{"saveAs": true}' --inject 'WeChat.css' --inject 'WeChat.js' --internal-urls ".*?" 'https://wx.qq.com/'
 
