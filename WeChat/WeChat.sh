591ad15b7dac2b1d9172a63814721675bde601a1
qvshuo <qvshuo@foxmail.com>
2021-12-05 19:07:55 +0800

    wechat css important

--- WeChat/WeChat.sh
+++ WeChat/WeChat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nativefier --icon 'WeChat.png' --name 'WeChat' --single-instance --file-download-options '{"saveAs": true}' --inject 'WeChat.css' --inject 'WeChat.js' --internal-urls ".*?" 'https://wx.qq.com/'
+nativefier --icon 'WeChat.png' --name 'WeChat' --single-instance --tray --width 960 --height 600 --file-download-options '{"saveAs": true}' --inject 'WeChat.css' --inject 'WeChat.js' --internal-urls ".*?" 'https://wx.qq.com/'
 
 tar -zcvf WeChat-linux-x64.tar.gz WeChat-linux-x64
