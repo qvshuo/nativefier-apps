db07c5e39ed7a5d109a5c4bee588915b113a5f79
qvshuo <qvshuo@foxmail.com>
2021-12-09 21:52:25 +0800

    yesplaymusic width height

--- YesPlayMusic/YesPlayMusic.sh
+++ YesPlayMusic/YesPlayMusic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nativefier --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --single-instance 'https://yes-play-music-gules-five.vercel.app/'
+nativefier --icon 'YesPlayMusic.png' --name 'YesPlayMusic' --single-instance --width 1120 --height 700 'https://yes-play-music-gules-five.vercel.app/'
 
 tar -zcvf YesPlayMusic-linux-x64.tar.gz YesPlayMusic-linux-x64
